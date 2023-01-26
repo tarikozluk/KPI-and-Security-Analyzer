7a4dc1c41d530bca387a9ceb1627e6ec917f2da9
tarikozluk <tarikozluk@gmail.com>
2023-01-26 14:03:49 +0300

    Elastic Factpry Directory editted

--- Elasticsearch_Factory/elasticsearch_logging.py
+++ Elasticsearch_Factory/elasticsearch_logging.py
@@ -20,7 +20,6 @@ def company_name_create(company,company_id):
             'company_id': '{}'.format(company_id),
             'LogDate': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
             '@timestamp': datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
         })
 
 def log_getter(company, command, db,username,chat_title):
@@ -40,4 +39,25 @@ def log_getter(company, command, db,username,chat_title):
             'LogDate': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
             'Group Name' : '{}'.format(chat_title),
             'Command': '{}'.format(command)
+        })
+
+
+
+def elastic_monthly_log_count(info_count,warning_count,medium_count,high_count,critical_count):
+    print("Connection Okeydir")
+    load_dotenv()
+    elastic_url = os.getenv("ELASTIC_URL")
+    elastic_user = os.getenv("ELASTIC_USER")
+    elastic_pass = os.getenv("ELASTIC_PASSWORD")
+    es = Elasticsearch([elastic_url], basic_auth=(elastic_user, elastic_pass))
+    es.index(
+        index='monthly_log_count_{}'.format(datetime.now().strftime("%Y.%m.%d")),
+        document={
+            'info_count': '{}'.format(info_count),
+            'warning_count_medium_count': '{}'.format(warning_count),
+            'high_count': '{}'.format(high_count),
+            'medium_count': '{}'.format(medium_count),
+            'critical_count': '{}'.format(critical_count),
+            'LogDate': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            '@timestamp': datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         })
